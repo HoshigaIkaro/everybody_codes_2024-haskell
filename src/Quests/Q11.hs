01245767cb8219168e7bcfd2cfe6b9efbbcf901c
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2024-11-30 21:22:52 -0500

    initial state for quest 11

---
+++ src/Quests/Q11.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -Wno-x-partial #-}
+
+module Quests.Q11 (run, part1, part2, part3) where
+
+run :: IO ()
+run = mempty
+
+part1 :: String -> String
+part1 = id
+
+part2 :: String -> String
+part2 = id
+
+part3 :: String -> String
+part3 = id
