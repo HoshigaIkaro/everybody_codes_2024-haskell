0700bff926424cbcf8404aef8e8ca576a252536d
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2024-12-11 22:08:32 -0500

    apply hlint

--- src/Quests/Q17.hs
+++ src/Quests/Q17.hs
@@ -6,8 +6,6 @@ import Data.Function
 import Data.List
 import Data.Map (Map)
 import Data.Map qualified as M
-import Data.Set (Set)
-import Data.Set qualified as S
 import Data.Text qualified as T
 
 run :: IO ()
@@ -37,23 +35,23 @@ pStars s = stars
     stars = concatMap (map fst . uncurry processRow) $ zip [0 ..] ls
 
 minDistToStarInConst :: Point -> Map Point [Point] -> (Point, Point, Int)
-minDistToStarInConst point = minimumBy (compare `on` (^. _3)) . map ((,,) <$> (const point) <*> id <*> dist point) . M.keys
+minDistToStarInConst point = minimumBy (compare `on` (^. _3)) . map ((,,) <$> const point <*> id <*> dist point) . M.keys
 
 mAddStar :: (Map Point [Point], Int) -> [Point] -> (Map Point [Point], Int)
 mAddStar res [] = res
 mAddStar (mapping, currentDist) rest = mAddStar (newMapping, currentDist + extraDist) newRest
   where
-    (point, closest, extraDist) = minimumBy (compare `on` (^. _3)) $ map (flip minDistToStarInConst mapping) rest
+    (point, closest, extraDist) = minimumBy (compare `on` (^. _3)) $ map (`minDistToStarInConst` mapping) rest
     f a = Just . maybe [a] (a :)
     updated k v = M.alter (f v) k
     newMapping = updated point closest (updated closest point mapping)
     newRest = rest \\ [point]
 
 makeConstellationAndDist :: [Point] -> (Map Point [Point], Int)
-makeConstellationAndDist = (mAddStar <$> ((,0) . (M.fromList . map (,[]) . take 1)) <*> (drop 1))
+makeConstellationAndDist = mAddStar <$> ((,0) . (M.fromList . map (,[]) . take 1)) <*> drop 1
 
 constellationSize :: (Map Point [Point], Int) -> Int
-constellationSize = ((+) <$> (M.size . fst) <*> snd)
+constellationSize = (+) <$> (M.size . fst) <*> snd
 
 part1 :: String -> Int
 part1 = constellationSize . makeConstellationAndDist . pStars
@@ -61,25 +59,26 @@ part1 = constellationSize . makeConstellationAndDist . pStars
 part2 :: String -> Int
 part2 = constellationSize . makeConstellationAndDist . pStars
 
-mFindPoint :: [Point] -> [Point] -> Maybe Point
-mFindPoint cluster remaining = find (\p -> any ($ p) distFuncs) remaining
-  where
-    distFuncs = map (((< 6) .) . dist) cluster
-
 makeCluster :: [Point] -> Maybe ([Point], [Point])
 makeCluster [] = Nothing
 makeCluster (x : rest) = Just $ go [x] rest
   where
-    go current remaining = case mFindPoint current remaining of
-        Nothing -> (current, remaining)
-        Just point -> go (point : current) (delete point remaining)
+    go current remaining = case filter (\p -> any ($ p) distFuncs) remaining of
+        [] -> (current, remaining)
+        newStars -> go (newStars <> current) (remaining \\ newStars)
+      where
+        distFuncs = map (((< 6) .) . dist) current
 
 clusterStars :: [Point] -> [[Point]]
 clusterStars remaining = case makeCluster remaining of
     Nothing -> []
     Just (cluster, newRemaining) -> cluster : clusterStars newRemaining
 
--- go current (x:xs)
-
--- part3 :: String -> Int
-part3 = product . take 3 . sortOn negate . map (constellationSize . makeConstellationAndDist) . clusterStars . pStars
+part3 :: String -> Int
+part3 =
+    product
+        . take 3
+        . sortOn negate
+        . map (constellationSize . makeConstellationAndDist)
+        . clusterStars
+        . pStars
